0a019c47acc2e2168fadda3a753d04d7a6762ffa
Richard y Gema <Richard y Gema@LAPTOP-VH760KVI>
2022-05-16 09:13:18 +0200

    Examen

---
+++ EXAMEN_16M_PRIMO/src/coso/EXAMEN_16M_PRIMO.java
@@ -0,0 +1,18 @@
+package coso;
+import java.util.Scanner;
+
+public class EXAMEN_16M_PRIMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Buenos dias Ceinmark, soy Guillermo San Bartolome \n Introduce"
+				+ "un numero para saber si es primo o no:");
+		
+		Scanner entrada = new Scanner(System.in);
+		int numero = 0;
+		numero = entrada.nextInt();
+
+	}
+
+}
