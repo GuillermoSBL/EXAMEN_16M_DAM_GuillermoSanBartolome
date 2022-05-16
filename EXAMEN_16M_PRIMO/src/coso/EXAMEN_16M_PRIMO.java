c0b50b01e0b2be29d198f746af3027125edf4c0e
Richard y Gema <Richard y Gema@LAPTOP-VH760KVI>
2022-05-16 09:25:50 +0200

    cambios

--- EXAMEN_16M_PRIMO/src/coso/EXAMEN_16M_PRIMO.java
+++ EXAMEN_16M_PRIMO/src/coso/EXAMEN_16M_PRIMO.java
@@ -6,12 +6,36 @@ public class EXAMEN_16M_PRIMO {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		System.out.println("Buenos dias Ceinmark, soy Guillermo San Bartolome \n Introduce"
-				+ "un numero para saber si es primo o no:");
+				+ " un numero para saber si es primo o no:");
 		
 		Scanner entrada = new Scanner(System.in);
 		int numero = 0;
 		numero = entrada.nextInt();
+		
+		int contador = 0;
+		
+		for(int a = 1; a <= numero; a++)
+        {
+            if((numero % a) == 0)
+            {
+                contador++;
+            }
+        }
+ 
+        if(contador <= 2)
+        {
+            System.out.println("El " + numero + " es primo");
+        }else{
+            System.out.println("El " + numero + " no es primo");
+        }		
+		    for(int i = 1 ; i <=numero ; i++){
+		        if(numero%i==0){
+		            System.out.println("Estos son los numeros divisores del " + numero + ": " +i);
+		        }
+		    }
 
 	}
 
